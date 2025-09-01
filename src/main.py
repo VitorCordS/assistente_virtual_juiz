7c6c532f41cd8743da1eaa69f8b22adbc792a19a
Vitor Cordeiro <vcordeirosilva79@gmail.com>
2025-09-01 15:40:02 -0300

    add de Procfile e runtime

--- src/main.py
+++ src/main.py
@@ -51,6 +51,7 @@ def serve(path):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=False)
 
 
